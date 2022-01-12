45017892b217a7cad68abf1de8dc685cf89b1dbd
David Edmundson <kde@davidedmundson.co.uk>
2022-01-12 08:39:33 +0000

    Use shared contexts for plasmashell
    Shared contexts allow us to share textures and vertex buffers between
    contexts (windows) this allows us to reduce memory in popups and alike.
    
    To fully make use of this requires some follow up work in
    ManagedTextureNode for FrameSVG, but we should sitll get some OOTB.
    
    If shared contexts are not supported this fails gracefully - though we
    are relying on shared contexts within kwin for a release without issue.
    
    It also should fix WebGL inside WebViews.
    
    BUG: 448299

--- shell/main.cpp
+++ shell/main.cpp
@@ -72,6 +72,7 @@ int main(int argc, char *argv[])
     } else {
         QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     }
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
     QQuickWindow::setDefaultAlphaBuffer(true);
 
